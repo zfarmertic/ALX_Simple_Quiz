ac81979e163538923123dce71baaeb6c16c96a70
zfarmertic <lilyigbunu@gmail.com>
2025-12-03 19:58:34 +0100

    quiz

---
+++ quiz.js
@@ -0,0 +1,21 @@
+function checkAnswer (){
+    let correctAnswer = "4";
+
+    const quiz = document.querySelector('input[name="quiz"]:checked');
+
+    console.log(quiz)
+    const userAnswer = quiz.value;
+
+    
+    if(userAnswer === correctAnswer){
+        const feedback = document.getElementById("feedback");
+        feedback.textContent = "Correct! Well done."
+    }else {
+        feedback.textContent = "That's incorrect. Try again!"
+    }
+    console.log(userAnswer);
+}
+
+const btn = document.getElementById("submit-answer")
+
+btn.addEventListener("click", checkAnswer)
